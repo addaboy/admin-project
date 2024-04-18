df7228c955e6c4e3d0dcfac6d4c7863572af8c0b
addaboy <adisubiyana@gmail.com>
2024-04-19 02:01:35 +0300

    New Panel

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+// import SignUp from './Authenticate/SignUp'
+import SignIn from './Authenticate/SignIn'
+import Register from './pages/Register'
+import './App.css'
+
+import Home from './Components/Home'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='./Authenticate/SignIn.jsx' element={<SignIn />} />
+        <Route path='./pages/Register.jsx' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+    
+    </>
+
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from './Footer'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+    <Sidebar />
+    <Footer />
+    </>
+  )
+}
+
+export default Home
